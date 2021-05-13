25c5c58211da9079427fffdbc8135560b1c0e0ed
EnKorona <enkorona@gmail.com>
2021-05-13 09:10:30 +0800

    更新路由

--- src/main/java/com/enkorona/quafoo/controller/VisionController.java
+++ src/main/java/com/enkorona/quafoo/controller/VisionController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/vision")
 public class VisionController {
-    @RequestMapping("/vision")
+    @RequestMapping("/hello")
     public String index() {
         return "Hello Vision";
     }
